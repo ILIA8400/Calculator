f75b24c7309c49216f5f834042b3c9b7b4702f64
ilia <iliaasgharhorri@gmail.com>
2024-07-03 16:07:37 +0330

    No chain calculation

--- script.js
+++ script.js
@@ -2,59 +2,65 @@ const $ = document;
 const input = $.querySelector("input");
 let value = input.value;
 
-let number ="";
-let number2 = "";
-let result = "";
-
+let operator = "";
+let number =0;
+let result = 0;
+let newOperation = false;
 
 
 $.querySelector("#zero").addEventListener("click",()=>{
-    value += "0";
+    value += 0;
     input.value = value;
 });
 
 $.querySelector("#one").addEventListener("click",()=>{
-    value += "1";
+    value += 1;
     input.value = value;
 });
 
 $.querySelector("#two").addEventListener("click",()=>{
-    value += "2";
+    value += 2;
     input.value = value;
 });
 
 $.querySelector("#three").addEventListener("click",()=>{
-    value += "3";
+    value += 3;
     input.value = value;
 });
 
 $.querySelector("#four").addEventListener("click",()=>{
-    value += "4";
+    value += 4;
     input.value = value;
 });
 
 $.querySelector("#five").addEventListener("click",()=>{
-    value += "5";
+    value += 5;
     input.value = value;
 });
 
 $.querySelector("#six").addEventListener("click",()=>{
-    value += "6";
+    value += 6;
     input.value = value;
 });
 
 $.querySelector("#seven").addEventListener("click",()=>{
-    value += "7";
+    value += 7;
     input.value = value;
 });
 
 $.querySelector("#eight").addEventListener("click",()=>{
-    value += "8";
+    value += 8;
     input.value = value;
 });
 
 $.querySelector("#nine").addEventListener("click",()=>{
-    value += "9";
+    value += 9;
+    input.value = value;
+});
+
+
+$.querySelector("#dot").addEventListener("click",()=>{
+    value += ".";
     input.value = value;
 });
 
@@ -62,45 +68,67 @@ $.querySelector("#nine").addEventListener("click",()=>{
 
 
 $.querySelector("#clear").addEventListener("click",()=>{
-    value = "";
-    number = "";
-    input.value = value;
+    number = 0;
+    input.value = "";
+    value = input.value;
 });
+
 
 //=============================================
 
 
 $.querySelector("#plus").addEventListener("click",()=>{
-    number = value;
+   
+    number = parseFloat(value);
+    operator = "plus";
     value = "";
     input.value = value;
+    
 });
 
 $.querySelector("#minus").addEventListener("click",()=>{
+    number = parseFloat(value);
+    operator = "minus";
     value = "";
     input.value = value;
 });
 
 $.querySelector("#times").addEventListener("click",()=>{
+    number = parseFloat(value);
+    operator = "times";
     value = "";
     input.value = value;
 });
 
 $.querySelector("#division").addEventListener("click",()=>{
+    number = parseFloat(value);
+    operator = "division";
     value = "";
     input.value = value;
 });
 
 $.querySelector("#equ").addEventListener("click",()=>{
-    
-    input.value = number;
+
+    calc();
+    number = 0;
+    input.value = result;
+    value = input.value;
 });
 
-//=======================
 
-function calculator(){
-    
-    result = number + input.value
-    
-    return result;
+//=============================
+
+
+function calc(){
+
+    if(operator == "plus")
+        result = parseFloat(input.value) + number;
+    else if(operator == "minus")
+        result = number - parseFloat(input.value);
+    else if(operator == "times")
+        result = number * parseFloat(input.value);
+    else if(operator == "division")
+        result = number / parseFloat(input.value);
+
+    operator = "";
 }
